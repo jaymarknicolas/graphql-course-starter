78821592535d9fb530a42f0c95ea1a4d21273f0f
jaymarknicolas <jmnicolas4me@gmail.com>
2025-11-28 07:10:49 +0800

    partial updates for query

--- server/index.ts
+++ server/index.ts
@@ -1,8 +1,28 @@
 import express from "express";
+import { ApolloServer } from "@apollo/server";
+import { expressMiddleware } from "@apollo/server/express4";
+import cors from "cors";
+import cookiParser from "cookie-parser";
+import schema from "./schema";
 
 const app = express();
 
+const server = new ApolloServer(schema);
+
 async function startServer() {
+  await server.start();
+
+  app.use(
+    "/graphql",
+    cors({
+      origin: "http://localhost:3000",
+      credentials: true,
+    }),
+    cookiParser(),
+    express.json(),
+    expressMiddleware(server)
+  );
+
   app.listen(4000, () => {
     console.log("Server is running on http://localhost:4000/graphql");
   });
